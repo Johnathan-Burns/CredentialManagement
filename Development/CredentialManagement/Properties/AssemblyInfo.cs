2c4108849417614e3241f7800562baa956c9aa3b
ilyalozovyy <ilyalozovyy@localhost>
2014-12-13 22:53:42 -0500

    Added Global Suppressions Updated version number Rebuilding to upload a new NuGet package with bug fixes.

--- Development/CredentialManagement/Properties/AssemblyInfo.cs
+++ Development/CredentialManagement/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("iLya Lozovyy")]
 [assembly: AssemblyProduct("CredentialManagement")]
-[assembly: AssemblyCopyright("Copyright © iLya Lozovyy 2011")]
+[assembly: AssemblyCopyright("Copyright © iLya Lozovyy 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
 [assembly: InternalsVisibleTo("WindowsCredentialsManagement.Tests")]
 [assembly: InternalsVisibleTo("WindowsCredentialsManagement.Explorables")]
